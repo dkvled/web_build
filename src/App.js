fbe08ebe617dde4f9dd3a31454bb0bf2a214a219
dkvled <123858201+dkvled@users.noreply.github.com>
2023-04-03 20:21:40 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Login from "./Login";
 import Signup from "./Signup";
-import PasswordChange from "./PasswordChange";
-import FindIdPage from "./FindIdPage";
 
 function App() {
   return ( 
     <div >
-      <FindIdPage />
+      <Signup />
     </div>
   );
 }
