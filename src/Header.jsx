f854603d5380e2412144416d67b5f6a5461f8591
dkvled <123858201+dkvled@users.noreply.github.com>
2023-04-12 18:18:12 +0900

    Create Header.jsx

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <button>로그인</button>
+      <button>회원가입</button>
+    </div>
+  );
+};
+
+export default Header;
