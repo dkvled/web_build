4954c27fab4afea69f6601c65b70d9c8db14d99a
dkvled <123858201+dkvled@users.noreply.github.com>
2023-05-09 17:39:47 +0900

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className="header">
+    <div className="header1">
       <div className="logo">
         <h2>웹 메이커스</h2>
       </div>
       <Link to="/login">로그인</Link>
       <Link to="/signup">회원가입</Link>
+      <Link to="/profile">프로필</Link>
+      
     </div>
   );
 };
